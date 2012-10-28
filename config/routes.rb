5ebd103ff5409bc6138547fa4b5fafe0e329216c
leonguan <htz0426@yahoo.com>
2012-10-28 11:35:40 -0700

    Finish sign in/out

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ NewsReader::Application.routes.draw do
   
   root :to => "articles#index"
 
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
