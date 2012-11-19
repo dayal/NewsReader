7c54d3c11bf1768bd0459b2bb35a34b4a08146b2
David Lu <daya_l@qq.com>
2012-11-18 16:09:04 -0800

    fix validations

--- app/models/favorite_list.rb
+++ app/models/favorite_list.rb
@@ -2,5 +2,5 @@ class FavoriteList < ActiveRecord::Base
   belongs_to :users
   has_and_belongs_to_many :articles
 
-  validates :user_id, :uniqueness => true
+  validates :user_id, presence: true, uniqueness: true
 end
--- app/models/news_list.rb
+++ app/models/news_list.rb
@@ -2,5 +2,5 @@ class NewsList < ActiveRecord::Base
   has_and_belongs_to_many :feeds
   attr_accessible :is_private
 
-  validates :user_id
+  validates :user_id, presence: true
 end
