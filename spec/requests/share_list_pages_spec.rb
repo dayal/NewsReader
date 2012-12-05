b101e9e222af160419c5fbd38b30efd547aaa4d0
leonguan <htz0426@yahoo.com>
2012-12-04 20:28:58 -0800

    shared list pages spec

---
+++ spec/requests/share_list_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "share list pages" do
+
+  subject { page }
+
+  describe "when user signed in" do
+
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in user }
+    let(:article) { FactoryGirl.create(:article) }
+
+    describe "create share list" do
+      before {visit article_path(article)}
+      it {should have_link('Share to Friends')}
+
+
+      describe "visit profile page without favorite list" do
+        before { visit user_path(user) }
+        it { should_not  have_selector('h5', text: 'Shared Articles') }
+      end
+
+      describe "visit profile page" do
+        before do
+          click_link 'Share to Friends'
+          visit user_path(user)
+        end
+        it { should have_selector('h5', text: 'Favorite Articles') }
+        it {should have_link(article.title)}
+        it {should have_link('Delete')}
+
+        describe "delete a article" do
+          before {click_link 'Delete'}
+          it { should_not  have_selector('h5', text: 'Share to Friends') }
+        end
+      end
+    end
+  end
+end
